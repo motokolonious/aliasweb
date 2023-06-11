7944d292e14c3270b33f0d32bc9889ece00a281c
lonious <gordlonious@gmail.com>
2023-06-11 04:41:02 -0400

    Try checking session before setting data.

--- server/php/index.php
+++ server/php/index.php
@@ -3,7 +3,9 @@
     session_start();
     $aliases = ['Lonious', 'Azerim', 'Azerah', 'Motoko', 'Mikasa', 'Bronn'];
     $rand_alias_index = array_rand($aliases);
-    $_SESSION['alias'] = $aliases[$rand_alias_index];
+    if (!array_key_exists('alias', $_SESSION) || strlen($_SESSION['alias']) == 0) {
+      $_SESSION['alias'] = $aliases[$rand_alias_index];
+    }
   }
 ?>
 
