bad662fa56ce025eaaf3bf9062e0e0c6b2e55039
lonious lit <lonious@gorkaltop.kaltop.net>
2024-11-29 00:21:08 -0500

    Add a file based token validator object.

---
+++ server/php/src/alias/crypt/token_validation.php
@@ -0,0 +1,34 @@
+<?php
+namespace alias\crypt;
+interface IValidateToken
+{
+    public function validate($token);
+    public const FILES_DIRECTORY_NAME = "token_files";
+}
+class FileTokenValidator implements IValidateToken
+{
+    public function validate($token)
+    {
+        //Relative paths work fine? Getting full paths is unnecessary?
+        /*$cwd = getcwd();
+        if ($cwd === false)
+        {
+            error_log("TOKEN_ERROR: Unable to get the current working directory.");
+            return false;
+        }*/
+        if (!file_exists(IValidateToken::FILES_DIRECTORY_NAME))
+        {
+            error_log("TOKEN_ERROR: The token directory does not exist.");
+            return false;
+        }
+        //Do I need to check for a maximum system file name length here? Trim?
+        $fp = IValidateToken::FILES_DIRECTORY_NAME . "/$token";
+        if (file_exists($fp)) return true;
+        return false;
+    }
+}
+/*$validator = new FileTokenValidator();
+$isValid = $validator->validate("FAKE_TOKEN");
+if ($isValid) echo "YEPP\n";
+else echo "NOPE\n";*/
+?>
