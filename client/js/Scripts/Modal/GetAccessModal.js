72994f5c9f42d0bc7b76db9ba44182158a451e9a
lonious <gordlonious@gmail.com>
2025-04-07 06:26:05 -0400

    Add GetAccessModal script alternative.

---
+++ client/js/Scripts/Modal/GetAccessModal.js
@@ -0,0 +1,28 @@
+function getAccessModal(headerText, paragraphText, submissionEndpoint) {
+  if (typeof headerText !== "string") throw new Error("GetAccessModal headerText must be a string.");
+  if (typeof paragraphText !== "string") throw new Error("GetAccessModal paragraphText must be a string.");
+  if (typeof submissionEndpoint !== "string") throw new Error("GetAccessModal submissionEndpoint must be a string.");
+  if (!submissionEndpoint.startsWith("https://aliasweb.me/api/")) throw new Error("GetAccessModal submissionEndpoint must start with {API}.");
+
+  //omg use caching function to count the instances and append the instance id?
+  //TODO: gaurantee that even when clients use this component multiple times in the same document that the input IDs are unique.
+  const AuthInputId = "getaccessmodal__input--m5caa68ips";
+
+  //this fn would then need to take an instanceId param?
+  function authenticate() {
+    const input = document.getElementById(AuthInputId);
+    if (input?.value === null || input?.value === undefined) {
+      throw new Error("Authentication failed! The input was not found.");
+    }
+    console.log('FAKE AUTHENTICATE SUCCESS!');
+  }
+
+  return `
+<article>
+  <header class="getaccessmodal__header">${headerText}</header>
+  <p class="getaccessmodal__paragraph">${paragraphText}</p>
+  <input id="${AuthInputId}" class="getaccessmodal__input" type="textbox" placeholder="..."/>
+  <input class="getaccessmodal__input" type="button" value="Authenticate" onclick="${function ocauthenticate() { authenticate(); }}" />
+</article>
+`;
+}
--- server/php/build/build_docroot.ps1
+++ server/php/build/build_docroot.ps1
@@ -1,23 +1,19 @@
 $createRoot = -Not (Test-Path "..\docroot\")
-if ($createRoot)
-{
-    New-Item -ItemType "Directory" -Path "..\docroot\"
-}
-else
-{
-    Remove-Item "..\docroot\*"
+if ($createRoot) {
+  New-Item -ItemType "Directory" -Path "..\docroot\"
+} else {
+  Remove-Item "..\docroot\*"
 }
 
-if (Test-Path "..\docroot")
-{
-    Copy-Item -Path "..\src\*.php" -Destination "..\docroot\"
-    Copy-Item -Path "..\..\..\client\htm\*.css" -Destination "..\docroot\"
-    if (Test-Path -Path "..\..\..\client\htm\aliasweb.png")
-    {
-        Copy-Item -Path "..\..\..\client\htm\aliasweb.png" -Destination "..\docroot\"
-    }
+if (-Not (Test-Path "..\docroot\")) {
+  Write-Host "There was a problem creating the docroot directory."
+  return 13
 }
-else
-{
-    Write-Host "There was a problem creating the docroot directory."
+
+Copy-Item -Path "..\src\*.php" -Destination "..\docroot\"
+Copy-Item -Path "..\..\..\client\htm\*.css" -Destination "..\docroot\"
+if (Test-Path -Path "..\..\..\client\htm\aliasweb.png") {
+  Copy-Item -Path "..\..\..\client\htm\aliasweb.png" -Destination "..\docroot\"
 }
+Copy-Item -Path "..\..\..\client\js\Modules\Modal\GetAccessModal.mjs" -Destination "..\docroot\"
+Copy-Item -Path "..\..\..\client\js\Scripts\Modal\GetAccessModal.js" -Destination "..\docroot\"
