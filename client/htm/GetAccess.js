ba67d92d2338e11c7023c4daec2a8061dfb97ccb
Motoko Lonious <ghaya@tuta.com>
2026-02-03 19:32:03 -0500

    Include auth modal back button.

--- client/htm/GetAccess.js
+++ client/htm/GetAccess.js
@@ -1,4 +1,5 @@
 function accessObjectFn() {
+  /*Declaring these as hidden HTMLs instead might be less code, but then parameterizing them would require additional dom mutations.*/
   const articleClassName = "getaccess__autharticle";
   const authenticateAccessModal = function authenticateAccessModal() { console.log("AUTHENTICATE"); };
   function getEightId() {
@@ -10,19 +11,38 @@ function accessObjectFn() {
     const lastFourChars = Array.from(lastFour).map(el => String.fromCharCode((parseInt(el) + 65))).reduce((accumulator, current) => accumulator + current);
     return firstFour + lastFourChars;
   }
+  function getBackBtnSvg() {
+    const svg = document.createElement("svg");
+    //svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("fill", "none");
+    svg.setAttribute("stroke", "#000000");
+    svg.setAttribute("stroke-width", "1");
+
+    const p1 = document.createElement("path");
+    p1.setAttribute("d", "M21 12h-12");
+    svg.appendChild(p1);
+
+    const p2 = document.createElement("path");
+    p2.setAttribute("d", "M13 16l-4 -4l4 -4");
+    svg.appendChild(p2);
+
+    const p3 = document.createElement("path");
+    p3.setAttribute("d", "M12 3a9 9 0 1 0 0 18");
+    svg.appendChild(p3);
+
+    return svg;
+  }
   return {
-    //Question(s): can good DOM mutations make it so that there is only ever one modal in the document? Why would you ever need more than one?
-
     articleClassName: articleClassName,
-
-    getAuthArticle: function getAccessElement(headerText, paragraphText, submissionEndpoint, divwrap) {
+    getAuthArticle: function getAccessElement(headerText, paragraphText, submissionEndpoint, divwrap, includeBackBtn) {
       if (typeof headerText !== "string") throw new Error("GetAccessModal headerText must be a string.");
       if (typeof paragraphText !== "string") throw new Error("GetAccessModal paragraphText must be a string.");
       if (typeof submissionEndpoint !== "string") throw new Error("GetAccessModal submissionEndpoint must be a string.");
       if (!submissionEndpoint.startsWith("https://aliasweb.me/api/")) throw new Error("GetAccessModal submissionEndpoint must start with {API}.");
 
-      const header = document.createElement("h4");
-      header.innerHTML = headerText;
+      const h4 = document.createElement("h4");
+      h4.innerHTML = headerText;
 
       const paragraph = document.createElement("p");
       paragraph.innerHTML = paragraphText;
@@ -37,7 +57,16 @@ function accessObjectFn() {
       const article = document.createElement("article");
       article.classList.add(articleClassName);
       article.setAttribute("id", articleId);
-      article.appendChild(header);
+      if (typeof includeBackBtn === "string" && includeBackBtn.length > 0) {
+        const header = document.createElement("header");
+        header.setAttribute("class", includeBackBtn);
+        header.appendChild(getBackBtnSvg());
+        header.appendChild(h4);
+        article.appendChild(header);
+      } else if (includeBackBtn) {
+        article.appendChild(getBackBtnSvg());
+        article.appendChild(h4);
+      } else article.appendChild(h4);
       article.appendChild(paragraph);
       article.appendChild(buttonInput);
       article.appendChild(textInput);
